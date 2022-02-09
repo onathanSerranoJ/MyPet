f3c45b931eba4651847866979d0c8f7c72095313
Jonathan <steelsouljs@gmail.com>
2022-02-09 08:42:39 -0800

    Added Frog class and its test Shrimp

---
+++ src/Hamster.java
@@ -0,0 +1,14 @@
+public class Hamster extends myPets
+{
+    //1 Instance variables
+        private boolean needToEx;//Do I need to exercise it
+        private boolean needsGrooming;//long hair vs short hair
+
+    //2 Constructors
+        public Hamster(boolean needToEx,boolean needsGrooming)
+        {
+
+        }
+    //3 toString
+
+}
